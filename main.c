be095b5574310bd970f566923d798c2c4258adf4
Natallya <natallya.estudos@gmail.com>
2024-12-02 15:42:04 -0300

    Feito a inserção da função cadastro do produto.

--- main.c
+++ main.c
@@ -19,3 +19,18 @@ void exibirMenu() {
     cout << "3. Sair" << endl;
     cout << "Escolha uma opção: ";
 }
+// Função de cadastro do produto 
+
+void cadastrarProduto(vector<Produto>& produtos) {
+    Produto produto;
+    cout << "\nDigite o ID do produto: ";
+    cin >> produto.id;
+    cin.ignore(); // Limpar o buffer
+    cout << "Digite o nome do produto: ";
+    getline(cin, produto.nome);
+    cout << "Digite o preço do produto: ";
+    cin >> produto.preco;
+
+    produtos.push_back(produto);
+    cout << "Produto cadastrado com sucesso!" << endl;
+}
